a4196de45ea99dac61ac2228c5ea9ad3b5f8d13a
hayas <hayas@qburst.com>
2019-03-02 13:26:38 +0530

    ss

--- server.js
+++ server.js
@@ -1,36 +1,9 @@
 var express = require("express");
 var bodyParser = require('body-parser');
 var login = require('./routes/loginRoutes');
-const session = require('express-session');
-const cors = require('cors');
-const errorHandler = require('errorhandler');
-const isProduction = process.env.NODE_ENV === 'production' || false;
-
 var app = express();
-app.use(cors());
-app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
-
-if(!isProduction) {
-  app.use(errorHandler());
-}
-
-//Error handlers & middlewares
-if(!isProduction) {
-  app.use((err, req, res) => {
-    res.status(err.status || 500);
-
-    res.json({
-      errors: {
-        message: err.message,
-        error: err,
-      },
-    });
-  });
-}
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
